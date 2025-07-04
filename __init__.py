9601c56052b27cbbfef272ab0ce09b2701a29b43
pvlprk <chasingtheangel@gmail.com>
2025-07-04 13:50:43 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,12 @@
 from .call_openai_assistant import CallAssistantNode
+from .comfydeploy_node import ComfyDeployNode
 
 NODE_CLASS_MAPPINGS = {
     "Call OpenAI Assistant": CallAssistantNode,
+    "ComfyDeploy API Caller": ComfyDeployNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Call OpenAI Assistant": "Call OpenAI Assistant",
+    "ComfyDeploy API Caller": "ComfyDeploy API Caller",
 }
