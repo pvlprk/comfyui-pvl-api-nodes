31d3d8c20a7d8d2f1c3e3cf346a9407588abb774
pvlprk <chasingtheangel@gmail.com>
2025-07-21 16:45:25 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
 from .pvl_call_openai_assistant import CallAssistantNode
 from .pvl_comfydeploy_node import ComfyDeployNode
 from .pvl_kontext_max import PvlKontextMax
+from .pvl_checkIfNone import IsConnected
 
 NODE_CLASS_MAPPINGS = {
     "PVL Call OpenAI Assistant": CallAssistantNode,
     "PVL ComfyDeploy API Caller": ComfyDeployNode,
     "PVL KONTEXT MAX": PvlKontextMax,
+    "PVLCheckIfConnected": IsConnected,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PVL Call OpenAI Assistant": "PVL Call OpenAI Assistant",
     "PVL ComfyDeploy API Caller": "PVL ComfyDeploy API Caller",
     "PVL KONTEXT MAX": "PVL KONTEXT MAX",
+    "PVLCheckIfConnected": "PVL Check If Connected",
 }
